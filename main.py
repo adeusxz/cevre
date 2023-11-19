4f70573bae915a14e5c851d4f2d77761b025764b
adeusxz <149710197+adeusxz@users.noreply.github.com>
2023-11-19 22:03:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import random, requests,os
 import discord
 from discord.ext import commands
-from envir import TOKEN
+from env import TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
-
-
 
 #selamlama
 @bot.command()
@@ -37,11 +35,12 @@ async def c1(ctx,cevap):
         skor+=20
     else: 
         await ctx.send(f'Yanlış:pensive:')
+        await ctx.send(f'Bir pil yaklaşık 4 metrekareyi kirletir. Lütfen yere atmayınız.')
         skor-=20
 
 @bot.command()
 async def s2(ctx):
-    await ctx.send(f'Soru 1) Bulaşık makinesi kaç litre su tüketir?')
+    await ctx.send(f'Soru 2) Bulaşık makinesi kaç litre su tüketir?')
     with open(r'images\bulasikmakinesi.png', 'rb') as f:
          picture = discord.File(f)
     await ctx.send(file=picture)
@@ -54,11 +53,12 @@ async def c2(ctx,cevap):
         skor+=20
     else: 
         await ctx.send(f'Yanlış:pensive:')
+        await ctx.send(f'Bulaşık makinesi 10 litre su tüketir. Lütfen elle bulaşık yıkamayınız')
         skor-=20
 
 @bot.command()
 async def s3(ctx):
-    await ctx.send(f'Soru 3) Hangi sera gazı atmosferde en fazla bulunur?')
+    await ctx.send(f'Soru 3) Araçlardan atmosfere salınan zehirli gaz nedir?')
     with open(r'images\atmosfer.jpg', 'rb') as f:
          picture = discord.File(f)
     await ctx.send(file=picture)
@@ -71,11 +71,12 @@ async def c3(ctx,cevap):
         skor+=20
     else: 
         await ctx.send(f'Yanlış:pensive:')
+        await ctx.send(f'Araçlardan atmosfere salınan gaz CO2. Lütfen daha çevreci olunuz.')
         skor-=20
 
 @bot.command()
 async def s4(ctx):
-    await ctx.send(f'Soru 4) Doğaya en çok zarar veren gaz hangisidir?')
+    await ctx.send(f'Soru 4) Doğadaki en zehirli gaz hangisidir?')
     with open(r'images\co2.jpg', 'rb') as f:
          picture = discord.File(f)
     await ctx.send(file=picture)
@@ -83,16 +84,17 @@ async def s4(ctx):
 @bot.command()
 async def c4(ctx,cevap):
     global skor
-    if cevap=="CO2":
+    if cevap=="CO":
         await ctx.send(f'Doğru:thumbsup:')
         skor+=20
     else: 
         await ctx.send(f'Yanlış:pensive:')
+        await ctx.send(f'Doğadaki en zehirli gaz CO')
         skor-=20
 
 @bot.command()
 async def s5(ctx):    
-    await ctx.send(f'Soru 5) Bir yılda Dünyada ortalama kaç metreküp su harcanır?')
+    await ctx.send(f'Soru 5) Bir yılda Dünyada kişibaşı ortalama kaç metreküp su harcanır?')
     with open(r'images\su.jpg', 'rb') as f:
          picture = discord.File(f)
     await ctx.send(file=picture)
@@ -100,12 +102,12 @@ async def s5(ctx):
 @bot.command()
 async def c5(ctx,cevap):
     global skor
-    if int (cevap)>=4000 and int (cevap) <=4500:
+    if int (cevap)>=800 and int (cevap) <=4500:
         await ctx.send(f'Doğru:thumbsup:')
         skor+=20
     else: 
         await ctx.send(f'Yanlış:pensive:')
-        await ctx.send(f'Bir yılda Dünyada ortalama 4000-4500 metreküp su harcanır')
+        await ctx.send(f'Bir yılda Dünyada kişibaşı ortalama 800 metreküp su harcanır')
         skor-=20
 
 @bot.command()
